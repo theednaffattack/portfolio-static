3cd9cdae03a7278c5e66e7fd99b79dd5640dc3b8
Eddie Naff <eddie@eddienaff.dev>
2022-03-22 08:35:29 -0700

    Experiment with JAM Sandwich style

---
+++ .eleventyPagesDir.js
@@ -0,0 +1,80 @@
+const { join } = require("path");
+// 1. import some new dependencies:
+// - Vite's build command
+// - requireFromString to turn that build output into a Node module
+// - React's createElement + renderToString to squeeze the static HTML out of our component
+const { build: viteBuild } = require("vite");
+const requireFromString = require("require-from-string");
+const { renderToString } = require("react-dom/server");
+const React = require("react");
+const mdxPlugin = require("@jamshop/eleventy-plugin-mdx");
+
+module.exports = function (eleventyConfig) {
+  // eleventyConfig.ignores.add("README.md");
+
+  // we'll use this to generate a unique ID for each component root (formerly just "root" in our create-react-app example)
+  let idCounter = 0;
+  eleventyConfig.on("beforeBuild", function () {
+    // reset the counter for each new build
+    idCounter = 0;
+  });
+
+  eleventyConfig.addPlugin(mdxPlugin);
+  eleventyConfig.addShortcode(
+    "react",
+    async function (componentPath, shipJavaScript) {
+      idCounter += 1;
+      const componentRootId = `component-root-${idCounter}`;
+      // generate an absolute path to our component, relative to that "_includes" directory for simplicity
+      const resolvedComponentPath = join(
+        process.cwd(),
+        "src/_includes",
+        componentPath
+      );
+      // 2. Call "build" in SSR mode and grab the output
+      const { output } = await viteBuild({
+        root: "_site",
+        build: {
+          ssr: true,
+          // prevents Vite from generating an output file
+          write: false,
+          rollupOptions: {
+            input: resolvedComponentPath,
+          },
+        },
+      });
+      // 3. Use requireFromString to process the raw build output
+      const { default: Component } = requireFromString(output[0].code);
+      // 4. Use renderToString to grab our markup and throw it into our root <div>
+      const html = renderToString(React.createElement(Component));
+      return `
+<div id="${componentRootId}">${html}</div>
+${
+  shipJavaScript
+    ? `<script type="module">
+import Component from ${JSON.stringify(resolvedComponentPath)};
+import React from 'react';
+import ReactDOM from 'react-dom';
+const root = document.getElementById('${componentRootId}');
+ReactDOM.hydrate(React.createElement(Component), root);
+</script>`
+    : ""
+}
+
+`;
+    }
+  );
+
+  eleventyConfig.addJavaScriptFunction("PrintURL", function () {
+    return this.page.url;
+  });
+  console.log(process.cwd());
+  return {
+    dir: {
+      input: "src/pages",
+      output: "_site",
+    },
+
+    templateFormats: ["md", "mdx", "njk", "html", "liquid"],
+  };
+};
--- notes/notes.md
+++ notes/notes.md
@@ -19,3 +19,8 @@ https://stackoverflow.com/a/43527235/9448010
 https://developer.mozilla.org/en-US/docs/Learn/Forms/Form_validation#validating_forms_using_javascript
 
 https://gist.github.com/erikdstock/af2a99d89c695df22c085de59d8036c3
+
+## MDX shortcode thingy
+
+https://github.com/jamshop/eleventy-plugin-mdx/issues/1#issuecomment-958612445
+https://twitter.com/MikeRiethmuller/status/1454590808053534721
